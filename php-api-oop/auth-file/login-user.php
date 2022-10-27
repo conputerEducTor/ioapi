0bbc830332b13acea746b28f522f126d9298f745
Nirmal Avhad <80802776+conputerEducTor@users.noreply.github.com>
2022-10-27 18:47:02 +0530

    Add files via upload

---
+++ php-api-oop/auth-file/login-user.php
@@ -0,0 +1,64 @@
+<?php
+
+header('Access-Control-Allow-Origin:*');
+header('Access-Control-Allow-Method:POST');
+header('Content-Type:application/json');
+include '../database/Database.php';
+include '../vendor/autoload.php';
+
+use \Firebase\JWT\JWT;
+
+$obj = new Database();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $data = json_decode(file_get_contents("php://input", true));
+    $email = htmlentities($data->email);
+    $password = htmlentities($data->password);
+
+    $obj->select('users', '*', null, "email='{$email}'", null, null);
+    $datas = $obj->getResult();
+    foreach ($datas as $data) {
+        $id = $data['id'];
+        $email = $data['email'];
+        $name = $data['name'];
+        
+        if(!password_verify($password,$data['password']))
+        {
+            echo json_encode([
+                'status'=>0,
+                'message'=>'Invalid Credentials',
+        
+            ]);
+        }else{
+            $payload=[
+                'iss'=>'localhost',
+                'aud'=>'localhost',
+                'exp'=>time()+10000,
+                'data'=>[
+                    'id'=>$id,
+                    'name'=>$name,
+                    'email'=>$email,
+                    
+                ],
+    
+            ];
+            $secret_key="Nirmal Avhad";
+            $jwt=JWT::encode($payload,$secret_key,'HS256');
+            echo json_encode([
+                'status'=>1,
+                'jwt'=>$jwt,
+                'message'=>'Login Successful',
+        
+            ]);
+        }
+        
+        
+    }
+    
+}else{
+    echo json_encode([
+        'status'=>0,
+        'message'=>'Access Denied',
+
+    ]);
+}
